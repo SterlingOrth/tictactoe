a232258396a9009f84eb89dd46d7cbdc75e073c8
SterlingOrth <71182922+SterlingOrth@users.noreply.github.com>
2021-05-12 00:00:22 -0600

    X and O can now take turns

--- src/index.js
+++ src/index.js
@@ -10,18 +10,18 @@ function Square(props) {
     </button>
   );
 }
-class Square extends React.Component {
-    render() {
-      return (
-        <button 
-         className="square"
-         onClick={() => this.props.onClick()}
-        >
-         {this.props.value}
-        </button>
-      );
-    }
-  }
+// class Square extends React.Component {
+//     render() {
+//       return (
+//         <button 
+//          className="square"
+//          onClick={() => this.props.onClick()}
+//         >
+//          {this.props.value}
+//         </button>
+//       );
+//     }
+//   }
   
   class Board extends React.Component {
   
@@ -29,13 +29,17 @@ class Square extends React.Component {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
 
       handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({ 
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        });
       }
 
       renderSquare(i) {
